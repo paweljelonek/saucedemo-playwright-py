1a8681bc129c9618634201ed2aa8fa2b2e6fcfc2
Pawel Jelonek <dev@jazzcat.pl>
2026-06-06 12:14:08 +0200

    refactor: make LoginPage locators private for consistency

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,18 +6,18 @@ from utils.urls import URL
 class LoginPage:
     def __init__(self, page: Page) -> None:
         self.page = page
-        self.username_input: Locator = page.locator("#user-name")
-        self.password_input: Locator = page.locator("#password")
-        self.login_button: Locator = page.locator("#login-button")
-        self.error_container: Locator = page.locator("[data-test='error']")
+        self._username_input: Locator = page.locator("#user-name")
+        self._password_input: Locator = page.locator("#password")
+        self._login_button: Locator = page.locator("#login-button")
+        self._error_container: Locator = page.locator("[data-test='error']")
 
     def navigate(self) -> None:
         self.page.goto(URL.LOGIN)
 
     def login(self, username: str, password: str) -> None:
-        self.username_input.fill(username)
-        self.password_input.fill(password)
-        self.login_button.click()
+        self._username_input.fill(username)
+        self._password_input.fill(password)
+        self._login_button.click()
 
     def get_error_message(self) -> str:
-        return self.error_container.inner_text()
+        return self._error_container.inner_text()
